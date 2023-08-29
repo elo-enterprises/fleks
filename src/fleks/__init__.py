821d4064619f68a3ad13336ede34df373fe6a73d
mvr <matt@example.com>
2023-08-28 23:21:39 -0700

    docs, doc-tests, doc-rendering work; stubbing config class

--- src/fleks/__init__.py
+++ src/fleks/__init__.py
@@ -1,4 +1,5 @@
 """ fleks """
 from .plugin import Plugin  # noqa
 from .util.typing import classproperty  # noqa
-from .util import tagging # noqa
+from .util import typing, tagging # noqa 
+from .config import Config # noqa
